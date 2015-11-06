5970f58cc9552c16230462a21c0f55ba54edb206
cwade <courtneywade@gmail.com>
2015-11-06 09:10:32 -0500

    Require scipy

--- setup.py
+++ setup.py
@@ -12,5 +12,5 @@ setup(name='surveyhelper',
 	  author_email="pysurveyhelper@gmail.com",
 	  license='MIT',
 	  packages=['surveyhelper'],
-	  install_requires=['beautifulsoup4', 'pandas', 'jinja2', 'unidecode'],
+	  install_requires=['beautifulsoup4', 'pandas', 'jinja2', 'unidecode', 'scipy'],
 	  zip_safe=False)
